bf677f46c223910a53891eded9dffd7c8169eeb9
sukhraj123 <nolimit.grewal@gmail.com>
2026-02-17 07:16:17 -0800

    Your commit message

---
+++ src/data/user.js
@@ -0,0 +1,79 @@
+const INFO = {
+	main: {
+		title: "Julius K. Portfolio",
+		name: "Julius Keeze",
+		email: "keeze9gz@gmail.com",
+		logo: "../logo.jpg",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Website Developer & Designer",
+		description:
+			"Im a website developer and designer look for clients who want to build thier brand, and to create the vision the client wil love. lets work together and buld something amazing you can be proud to represent.",
+	},
+
+	about: {
+		title: "I’m Julius Keez. I live in Canada, where I design websites for brands that stand out.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Builders Website",
+			
+			logo: "../jp.png",
+			linkText: "View Project",
+			link: "https://jpbuilders.ca/",
+		},
+
+		{
+			title: "Boba Tea Website",
+			
+			logo: "../boba.png",
+			linkText: "View Project",
+			link: "https://sukhraj123.github.io/ilovecelsius/",
+		},
+
+		{
+			title: "Pizza Shop Website",
+			
+			logo: "../pizza.png",
+			linkText: "View Project",
+			link: "https://sukhraj123.github.io/Supremepizza/",
+		},
+
+		{
+			title: "Electrical & Reno Website",
+			
+			logo: "../gill.png",
+			linkText: "View Project",
+			link: "https://gillnoorelectricalandreno.ca/",
+		},
+
+		{
+			title: "Cannabis Website",
+			
+			logo: "../can.png",
+			linkText: "View Project",
+			link: "https://kzwebsolutions.github.io/joeindica/",
+		},
+	],
+};
+
+export default INFO;
